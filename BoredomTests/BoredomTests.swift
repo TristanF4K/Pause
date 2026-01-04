e61aff5d371608f5e813e53b3b00328b50abb31c
tristansrebot <tristan@f4k.de>
2026-01-04 15:11:33 +0100

    Initial Commit

---
+++ BoredomTests/BoredomTests.swift
@@ -0,0 +1,16 @@
+//
+//  BoredomTests.swift
+//  BoredomTests
+//
+//  Created by Tristan Srebot on 04.01.26.
+//
+
+import Testing
+
+struct BoredomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
